c8d67d16d73cd8b2b51220fa3d6b3f820145efe5
Xosa <anastasia.64k@gmail.com>
2025-07-20 19:23:51 +0300

    Fix BE url

--- client/src/config.js
+++ client/src/config.js
@@ -1,7 +1,7 @@
 const config = {
   API_BASE_URL:
     process.env.NODE_ENV === "production"
-      ? "https://anasa.vercel.app/api"
+      ? "https://anasa.vercel.app"
       : "http://localhost:5001",
 };
 
